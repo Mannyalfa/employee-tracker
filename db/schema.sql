d8b628cfe20f3bdad395ae8bb01830cdc39fafa4
Manny Alfaro <78244552+Mannyalfa@users.noreply.github.com>
2021-07-18 20:21:28 -0400

    updated schema for foriegn key constraints

--- db/schema.sql
+++ db/schema.sql
@@ -3,24 +3,24 @@ CREATE DATABASE employees;
 USE employees;
 
 CREATE TABLE department (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(30) NOT NULL
-  );
-
-  CREATE TABLE role (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    department_id INT NOT NULL,
-    FOREIGN KEY
-    (department_id) REFERENCES department (id)
-  );
-  
-    CREATE TABLE employee (
-      id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role (id), FOREIGN KEY
-      (manager_id) REFERENCES employee (id)
-  );
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
+);
+CREATE TABLE role (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  INDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+CREATE TABLE employee (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT UNSIGNED NOT NULL,
+  INDEX role_ind (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  manager_id INT UNSIGNED,
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,12 @@
 USE employees;
 
-INSERT INTO department (name)
-VALUES `
+INSERT INTO department (name) VALUES 
     ("Sales"),
     ("Engineering"),
     ("Finance"),
     ("Legal");
 
-INSERT INTO role (title, salary, department_id)
-VALUES
+INSERT INTO roles (title, salary, department_id) VALUES
     ("Sales Lead", 100000, 1),
     ("Salesperson", 80000, 1),
     ("Lead Engineer", 150000, 2),
@@ -17,8 +15,7 @@ VALUES
     ("Legal Team Lead", 250000, 4),
     ("Lawyer", 190000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id)
-VALUES
+INSERT INTO employee (first_name, last_name, role_id) VALUES
     ("John", "Doe", 1),
     ("Mike", "Chan", 2),
     ("Ashley", "Rodriguez", 3),
